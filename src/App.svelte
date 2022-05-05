0e90fc53339e928e3e742654b0ff3129784b2d1d
gri <grigoriymashkovtsev@gmail.com>
2022-05-05 21:52:28 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+  import Type from './components/Type.svelte';
+
+</script>
+
+<Type>HW</Type>
---
+++ src/components/Type.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+ import '../app.css'
+</script>
+
+<div class="p-4 text-blue-500">
+    <slot/>
+</div>
