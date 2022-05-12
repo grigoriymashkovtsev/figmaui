60c8915c82812606eaf63e3169af3d00ec6b3077
gri <grigoriymashkovtsev@gmail.com>
2022-05-12 23:50:08 +0300

    is it commit?

--- src/components/Type.svelte
+++ src/components/Type.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
- import '../app.css'
+import '../app.css';
+
+type FontWeight = 'BOLD' | 'MEDIUM';
+export let fontWeight: FontWeight = 'BOLD';
 </script>
 
-<div class="p-4 text-blue-500">
-    <slot/>
+<div style={fontWeight} class="p-4 text-blue-500">
+    <slot />
 </div>
